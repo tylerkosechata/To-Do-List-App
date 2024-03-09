224b1c23c9a1fe3b7d5edfa59acf1b1726f3afea
Tyler Kosechata <tykosechata@gmail.com>
2024-03-09 15:45:01 +0100

    Created and added js script

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function newItem() {
+    //1. Adding a new item to the list of items:
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
+  
+    if ($("#input").val() === "") {
+      alert("You must write something!");
+    } else {
+      let list = $("#list");
+      list.append(li);
+    }
+    //2. Crossing out an item from the list of items:
+    li.on("dblclick", function () {
+      li.toggleClass("strike");
+    });
+  
+    //3(i). Adding the delete button "X":
+    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    $(crossOutButton).append("X");
+    li.append(crossOutButton);
+  
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  
+    crossOutButton.on("click", function () {
+      li.addClass("delete");
+    });
+  
+    // 4. Reordering the items:
+    $("#list").sortable();
+  }
